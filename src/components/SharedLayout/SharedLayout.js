989dd1b45daaccd91bd2e8d5575664d5290d14b4
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-04-21 10:51:21 +0300

    finished styled component sharedLayout

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,14 +1,37 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { Container } from 'globalStyles/Global.styled';
+import {
+  Header,
+  ContainerContent,
+  ListNav,
+  Nav,
+  StyledLink,
+  ItemNav,
+} from './SharedLayout.styled';
+import logo from 'images/twitter.png';
 
 export const SharedLayout = () => {
   return (
     <>
-      <header>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/tweets">Tweets</NavLink>
-        </nav>
-      </header>
+      <Header>
+        <Container>
+          <ContainerContent>
+            <img width={40} src={logo} alt="logo" />
+            <Nav>
+              <ListNav>
+                <ItemNav>
+                  <StyledLink to="/">Home</StyledLink>
+                </ItemNav>
+                <ItemNav>
+                  <StyledLink to="/tweets" end>
+                    Tweets
+                  </StyledLink>
+                </ItemNav>
+              </ListNav>
+            </Nav>
+          </ContainerContent>
+        </Container>
+      </Header>
       <Outlet />
     </>
   );
